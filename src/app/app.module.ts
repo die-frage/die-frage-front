e054d8a780a60f25927ea5a86508ab4d4c554df3
jekasrs <jekasjeny1012@gmail.com>
2024-03-01 21:40:16 +0100

    header init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderUnauthorisedComponent } from './header-unauthorised/header-unauthorised.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderUnauthorisedComponent
   ],
   imports: [
     BrowserModule
