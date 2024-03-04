c47c454083336921d79eab1c3619500052cfd7ea
jekasrs <jekasjeny1012@gmail.com>
2024-03-04 23:45:33 +0100

    static header for auth user implemented

---
+++ src/app/header-authorised/header-authorised.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-authorised',
+  templateUrl: './header-authorised.component.html',
+  styleUrls: ['./header-authorised.component.css']
+})
+export class HeaderAuthorisedComponent {
+
+
+  toUserPage(){
+
+  }
+
+  toSurveysPage(){
+
+  }
+
+
+}
