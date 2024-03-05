c52ff0657b2420ef93aee68b3a8d5ae6d10145b1
jekasrs <jekasjeny1012@gmail.com>
2024-03-05 19:00:06 +0100

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { HeaderUnauthorisedComponent } from './header-unauthorised/header-unauthorised.component';
+import { HeaderUnauthorisedComponent } from './header/header-unauthorised/header-unauthorised.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { AuthPageComponent } from './auth-page/auth-page.component';
 import {provideRouter, RouterModule} from "@angular/router";
 import routeConfig from "./routes";
-import { HeaderAuthorisedComponent } from './header-authorised/header-authorised.component';
+import { HeaderAuthorisedComponent } from './header/header-authorised/header-authorised.component';
+import { HomeComponent } from './home/home.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HeaderAuthorisedComponent } from './header-authorised/header-authorised
     HeaderUnauthorisedComponent,
     RegisterPageComponent,
     AuthPageComponent,
-    HeaderAuthorisedComponent
+    HeaderAuthorisedComponent,
+    HomeComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
