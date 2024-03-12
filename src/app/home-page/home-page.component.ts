0ed83335fb69b2e0bb5099dbf37f8382340e253f
jekasrs <jekasjeny1012@gmail.com>
2024-03-12 14:17:37 +0100

    Realised add button and search item

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,17 +8,32 @@ import {TokenStorageService} from "../auth/token-storage.service";
 })
 export class HomePageComponent {
 
-  info: any;
+  // info: any;
 
-  constructor(private token: TokenStorageService ) {
+  constructor(private token: TokenStorageService) {
 
   }
 
   ngOnInit(){
-    this.info = {
-      username: this.token.getUserName(),
-      token: this.token.getToken()
+    // this.info = {
+    //   username: this.token.getUserName(),
+    //   token: this.token.getToken()
+    // }
+  }
+
+  onClickAddSurvey() {
+    console.log("clicked")
+  }
+
+  handleKeyPress(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      const inputValue = (event.target as HTMLInputElement).value;
+      // Pass inputValue to your function for further processing
+      console.log("Entered value:", inputValue);
     }
   }
+
+
+
 
 }
